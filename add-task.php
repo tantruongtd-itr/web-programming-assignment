1f3444997da43d9196b71a514b2b17ac92c8e746
Trương Trần Duy Tân <tantruongtd@itrvn.com>
2024-04-07 19:08:58 +0700

    update tasks

--- add-task.php
+++ add-task.php
@@ -2,4 +2,43 @@
 session_start();
 include_once('includes/header.php');
 ?>
-    
+
+<link rel = "stylesheet" href = "./public/css/add-task.css">
+
+<body>
+
+<div class = "container page-title-container">
+    <div class = "page-title">
+        Add Task
+    </div>
+</div>
+
+<div class = "container">
+  <form id="add-task-form">
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name" placeholder="Your name..">
+
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email" placeholder="Your email..">
+
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username" placeholder="Username..">
+
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" placeholder="Password..">
+  
+    <label for="role">role</label>
+    <select id="role" name="role">
+      <option value="ADMIN">Administrator</option>
+      <option value="Director">Director</option>
+      <option value="Heads">Department heads</option>
+      <option value="Staffs">Staffs</option>
+    </select>
+  
+    <input type="submit" value="Submit">
+  </form>
+</div>
+
+</body>
+
+
